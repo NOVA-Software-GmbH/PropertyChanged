5c3a8b9c81f462d586f2ba2042bfa89d0768ceac
AndreasMantoBAV <79461762+AndreasMantoBAV@users.noreply.github.com>
2021-02-22 17:00:02 +0100

    IWeaveINotifyPropertyChanged implementiert (#1)

---
+++ PropertyChanged.Fody/Config/OnlyWeaveIWeaveINotifyPropertyChangedConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Xml;
+
+public partial class ModuleWeaver
+{
+    public bool OnlyWeaveIWeaveINotifyPropertyChanged;
+
+    public void ResolveOnlyWeaveIWeaveINotifyPropertyChangedConfig()
+    {
+        var value = Config?.Attributes("OnlyWeaveIWeaveINotifyPropertyChanged")
+            .Select(a => a.Value)
+            .SingleOrDefault();
+        if (value != null)
+        {
+            OnlyWeaveIWeaveINotifyPropertyChanged = XmlConvert.ToBoolean(value.ToLowerInvariant());
+        }
+    }
+}
---
+++ PropertyChanged.Fody/WeaveINotifyPropertyChangedInterfaceFinder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+
+public partial class ModuleWeaver
+{
+    const string WeaveINotifyPropertyChangedName = "IWeaveINotifyPropertyChanged";
+    Dictionary<string, bool> typesImplementingIWeaveINotifyPropertyChanged = new Dictionary<string, bool>();
+
+    public bool HierarchyImplementsIWeaveINotifyPropertyChanged(TypeReference typeReference)
+    {
+        var fullName = typeReference.FullName;
+        if (typesImplementingIWeaveINotifyPropertyChanged.TryGetValue(fullName, out var implementsIWeaveINotifyPropertyChanged))
+        {
+            return implementsIWeaveINotifyPropertyChanged;
+        }
+
+        TypeDefinition typeDefinition;
+        if (typeReference.IsDefinition)
+        {
+            typeDefinition = (TypeDefinition)typeReference;
+        }
+        else
+        {
+            try
+            {
+                typeDefinition = Resolve(typeReference);
+            }
+            catch (Exception ex)
+            {
+                EmitWarning($"Ignoring type {fullName} in type hierarchy => {ex.Message}");
+                return false;
+            }
+        }
+
+        foreach (var interfaceImplementation in typeDefinition.Interfaces)
+        {
+            if (interfaceImplementation.InterfaceType.Name == WeaveINotifyPropertyChangedName)
+            {
+                typesImplementingIWeaveINotifyPropertyChanged[fullName] = true;
+                return true;
+            }
+        }
+
+        var baseType = typeDefinition.BaseType;
+        if (baseType == null)
+        {
+            typesImplementingIWeaveINotifyPropertyChanged[fullName] = false;
+            return false;
+        }
+
+        var baseTypeImplementsIWeaveINotifyPropertyChanged = HierarchyImplementsINotify(baseType);
+        typesImplementingIWeaveINotifyPropertyChanged[fullName] = baseTypeImplementsIWeaveINotifyPropertyChanged;
+        return baseTypeImplementsIWeaveINotifyPropertyChanged;
+    }
+}
