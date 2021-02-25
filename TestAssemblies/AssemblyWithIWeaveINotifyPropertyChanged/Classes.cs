0775d47462067d850c0277aa1b284e0a4c5a37b4
AndreasMantoBAV <79461762+AndreasMantoBAV@users.noreply.github.com>
2021-02-25 10:18:18 +0100

    Feature/bavlims 891 (#3)
    * IWeaveINotifyPropertyChanged implementiert
    
    * UnitTests erstellt
    
    * nuget package description angepasst.

---
+++ TestAssemblies/AssemblyWithIWeaveINotifyPropertyChanged/Classes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+public class ClassWithIWeaveINotifyPropertyChanged : IWeaveINotifyPropertyChanged
+{
+    public string Property1 { get; set; }
+    public string Property2 { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
+
+public class ClassWithINotifyPropertyChanged : INotifyPropertyChanged
+{
+    public string Property1 { get; set; }
+    public string Property2 { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
---
+++ Tests/OnlyWeaveIWeaveINotifyPropertyChangedConfigTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Linq;
+using Xunit;
+
+public class OnlyWeaveIWeaveINotifyPropertyChangedConfigTests
+{
+    [Fact]
+    public void False()
+    {
+        var xElement = XElement.Parse("<PropertyChanged OnlyWeaveIWeaveINotifyPropertyChanged='false'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveOnlyWeaveIWeaveINotifyPropertyChangedConfig();
+        Assert.False(moduleWeaver.OnlyWeaveIWeaveINotifyPropertyChanged);
+    }
+
+    [Fact]
+    public void False0()
+    {
+        var xElement = XElement.Parse("<PropertyChanged OnlyWeaveIWeaveINotifyPropertyChanged='0'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveOnlyWeaveIWeaveINotifyPropertyChangedConfig();
+        Assert.False(moduleWeaver.OnlyWeaveIWeaveINotifyPropertyChanged);
+    }
+
+    [Fact]
+    public void True()
+    {
+        var xElement = XElement.Parse("<PropertyChanged OnlyWeaveIWeaveINotifyPropertyChanged='True'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveOnlyWeaveIWeaveINotifyPropertyChangedConfig();
+        Assert.True(moduleWeaver.OnlyWeaveIWeaveINotifyPropertyChanged);
+    }
+
+    [Fact]
+    public void True1()
+    {
+        var xElement = XElement.Parse("<PropertyChanged OnlyWeaveIWeaveINotifyPropertyChanged='1'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveOnlyWeaveIWeaveINotifyPropertyChangedConfig();
+        Assert.True(moduleWeaver.OnlyWeaveIWeaveINotifyPropertyChanged);
+    }
+
+    [Fact]
+    public void Default()
+    {
+        var moduleWeaver = new ModuleWeaver();
+        moduleWeaver.ResolveOnlyWeaveIWeaveINotifyPropertyChangedConfig();
+        Assert.False(moduleWeaver.OnlyWeaveIWeaveINotifyPropertyChanged);
+    }
+}
